428a58f880f6ba0ea65d1c1096d17ab3c42c5542
Carl <cs462@kent.ac.uk>
2017-04-09 12:29:02 +0100

    Remove es6 dependencies + cleanup codebase

--- jsonapi-rehydrate.js
+++ jsonapi-rehydrate.js
@@ -3,69 +3,127 @@
  *
  * @author Carl
  * @version 0.0.1
+ *
  * use as data = jsonApiRehydrate( json_API_response );
  */
 (function(){
-	let relationMap = {}; 
-	let hydratedItemMap = {}
+	"use strict";
 
-	// Hydrate an item, combine all its data + include the relations as part of rthe data set
-	function hydrate(item){
-		var newItem;
-		var key = item.type+':'+item.id;
-		if(typeof hydratedItemMap[key] === 'object'){
-			newItem = hydratedItemMap[key];
-		}else{
-			newItem = {};
-			newItem = Object.assign(newItem,item.attributes);
-			newItem.id = item.id;
+	/**
+	 * json:api Re-hydrater
+	 * This is responsible for all the heavy lifting involved in rehydrating a json:api response
+	 */ 
+	var jsonReHydrater = function(){
+		// Store relations for quick lookup
+		this.relationMap = {}; 
+		// Keep tract of hydrated items for reuse
+		this.hydratedItemMap = {};
 
-			hydratedItemMap[key] = newItem;
-		}
+		/**
+		 * Attempt to rehydrate this item, flatten json:api structure a little
+		 * and reattach all of its relations
+		 */ 
+		this.hydrate = function(data){
+			var item, key = data.type+':'+data.id;
 
-		for(let relName in item.relationships){
-			let relation = item.relationships[relName];
-			newItem[relName] = loadRelation(relation.data);
-		}
-		return newItem;
-	}
+			// Create basic data
+			item = data.attributes;
+			item.id = data.id;
 
-	function loadRelation(relation){
-		if(Array.isArray(relation)){
-			let collection = [];
-			for(let rel of relation){
-				collection.push(loadRelation(rel));
+			// Add to map
+			this.hydratedItemMap[key] = item;
+
+			// Apply relations
+			for(var relName in data.relationships){
+				if(data.relationships.hasOwnProperty(relName)){
+					var relation = data.relationships[relName];
+					item[relName] = this.loadRelation(relation.data);
+				}
 			}
-			return collection;
-		}
+			return item;
+		};
 
-		if(hydratedItemMap[relation.type+':'+relation.id]){
-			return hydratedItemMap[relation.type+':'+relation.id];
-		}else{
-			return hydrate(relationMap[relation.type+':'+relation.id]);
-		}
-	}
+		/**
+		 * Load a given relation. Can be either singular or a collection
+		 */ 
+		this.loadRelation = function(relation){
+			// If this is a collection relation, call self with each item & return em all as an array
+			if(Array.isArray(relation)){
+				var collection = [];
 
-	// Make map of includes for quick lookup
-	function generateIncludeMap(included){
-		for(let item of included){
-			relationMap[item.type+':'+item.id] = item; 
-		}
-	}
-
-	// public
-	window.jsonApiRehydrate = function(data){
-
-		// Make includes map
-		generateIncludeMap(data.included);
-
-		if(Array.isArray(data.data)){
-			let collection = [];
-			for(let item of data.data){
-				collection.push(hydrate(item));
+				for(var r in relation){
+					if(relation.hasOwnProperty(r)){
+						collection.push(this.loadRelation(relation[r]));
+					}
+				}
+				return collection;
 			}
-			return collection;
-		}
-		return hydrate(data.data);
+
+			var key = relation.type+':'+relation.id;
+			// Check if we have loaded this already, if not grab it from the map & hydrate it
+			if(this.hydratedItemMap[key]){
+				return this.hydratedItemMap[key];
+			}else{
+				return this.hydrate(this.relationMap[key]);
+			}
+		};
+
+		/**
+		 * Make a quick lookup map for includes so we can access them without looping
+		 */ 
+		this.generateIncludeMap = function(included){
+			var i, item;
+			for(i in included){
+				if(included.hasOwnProperty(i)){
+					item = included[i];
+					this.relationMap[item.type+':'+item.id] = item; 
+				}
+			}
+		};
+
+		/**
+		 * Parse json:api payload & return rehydrated object
+		 */ 
+		this.parse = function(payload){
+			// Create quick lookup for includes
+			this.generateIncludeMap(payload.included);
+
+			// Hydrate the main data set (if collection)
+			if(Array.isArray(payload.data)){
+				var collection = [];
+
+				for(var item in payload.data){
+					if(payload.data.hasOwnProperty(item)){
+						collection.push(this.hydrate(payload.data[item]));
+					}
+				}
+				return collection;
+			}
+			// or of singular
+			return this.hydrate(payload.data);	
+		};
 	};
+
+	/**
+	 * jsonReHydratePublic
+	 * Public api for the json:api rehydrater
+	 */ 
+	var jsonApiReHydratePublic = {};
+	jsonApiReHydratePublic.rehydrate = function(payload){
+		var jrh = new jsonReHydrater();
+		return jrh.parse(payload);	
+	};
+
+	// Add ourselves to the outside world / global name-space
+	if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+		module.exports = jsonApiReHydratePublic;
+	} else {
+		if (typeof define === "function" && define.amd) {
+			define([], function() {
+				return jsonApiReHydratePublic;
+			});
+		} else {
+			window.jsonApiReHydrate = jsonApiReHydratePublic;
+		}
+	}
 })();
